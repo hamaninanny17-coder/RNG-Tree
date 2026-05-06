14ccb488fdb94644874dbd7af4f689d2f1bd7479
hamaninanny17-coder <hamaninanny17@gmail.com>
2026-05-06 14:34:26 +0300

    rng.js

--- js/utils/rng.js
+++ js/utils/rng.js
@@ -19,7 +19,7 @@ const RNG_DATA = {
 		let l = Math.max(Math.min(Math.floor(random(getSeed()*row)*RNG_DATA.maxLayers+1), RNG_DATA.maxLayers), RNG_DATA.minLayers);
 		return Math.min(l, row);		
 	},
-	chars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'.split(''),
+	chars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()<.>?{}[]|`~'.split(''),
 	types: ["normal", "static"],
 	rowReqs: {
 		1: new Decimal(10),
